1bf8422bd9a5b0db2d6953abd604d00d7cc887e0
deividspiv <deividsp.iv@gmail.com>
2026-06-05 20:25:33 -0600

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import requests
 
 # --- LLAVES DE TU PROYECTO (Supabase) ---
-SUPABASE_URL = "https://fdhjanrmfgevuxzersnd.supabase.co" 
+SUPABASE_URL = "https://pmbrvgmvvrxtsmkmqfcg.supabase.co/rest/v1/" 
 
 # ⚠️ IMPORTANTE: Ve a Supabase -> Project Settings -> API
 # Copia la llave secreta llamada 'service_role' (NO la anon) y pégala aquí abajo.
